23cbb4b915dbd384a573e864fed6a569856a287d
mdJesan-08 <jesan12008124@gmail.com>
2026-01-22 22:41:20 +0600

    milestone 03 added

---
+++ Milestone-03/basicjavaScript.js
@@ -0,0 +1,17 @@
+var car = "Bugati";
+
+var h;
+// console.log(car);
+// console.log(h);
+// console.log(typeof(car));
+var a = 23;
+var st = parseInt('abc');
+var num = parseFloat("22.564")
+console.log(st)
+console.log(num);
+ num = parseInt("22.564")
+console.log(num);
+console.log(typeof(a));
+
+
+
