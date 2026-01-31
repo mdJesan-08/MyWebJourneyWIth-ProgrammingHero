5f8625b56bf5ecf41ff3ba2a26ac0f690b2c7692
mdJesan-08 <jesan12008124@gmail.com>
2026-01-31 19:34:54 +0600

    array learned

---
+++ Milestone-03/arrayWithLoop.js
@@ -0,0 +1,16 @@
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+
+function sumOfNum(numbers)
+{
+    let sum = 0;
+    for(let i = 0; i < numbers.length; i++)
+    {
+        sum += numbers[i];
+        
+    }
+    return sum;
+
+}
+
+console.log(sumOfNum(numbers));
