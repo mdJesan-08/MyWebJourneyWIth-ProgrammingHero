81c3d0613b2d8c7e0858909f07cf27d48105bc93
mdJesan-08 <jesan12008124@gmail.com>
2026-02-02 09:46:19 +0600

    BulbOnOffProject

--- Milestone-04/BulbOnOFF/script.js
+++ Milestone-04/BulbOnOFF/script.js
@@ -1,4 +1,23 @@
 let bulb = document.querySelector(".light-bulb");
+let btn = document.querySelector("button");
 
+
+let flag = 0;
+
+btn.addEventListener("click", function()
+    {
+        if(flag === 0)
+        {
+            bulb.style.backgroundColor = "rgb(21, 209, 71)";
+            flag = 1;
+        }
+        else 
+        {
+            bulb.style.backgroundColor = "yellow";
+            flag = 0;           
+        }
+
+
+    })
 
 
